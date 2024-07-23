83d262bf9588b6fba42cd18ef0f6208feb9b548b
wang <pvvquang98@gmail.com>
2024-07-23 23:02:59 +0700

    init commit

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -8,14 +8,24 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-   const register = async (userData) => {
+    const register = async (userData) => {
         try {
-            const response = await registerUser(userData);
-            setUser(response.data.user);
+            await registerUser(userData);
+            const loginResponse = await loginUser({ 
+                username: userData.username, 
+                password: userData.password 
+            });
+            const token = loginResponse.data.access;
+            localStorage.setItem('token', token); // Save token in localStorage
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`; // Set token for subsequent requests
+
+            // Fetch user profile after registration
+            const userProfileResponse = await axios.get('http://127.0.0.1:8000/api/auth/user/');
+            setUser(userProfileResponse.data);
             setIsAuthenticated(true);
         } catch (error) {
             console.error('Đăng ký thất bại', error);
-            throw error;
+            setIsAuthenticated(false);
         }
     };
 
@@ -44,21 +54,21 @@ export const AuthProvider = ({ children }) => {
         window.location.reload();
     };
 
-    // useEffect(() => {
-    //     const token = localStorage.getItem('token');
-    //     if (token) {
-    //         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    //         axios.get('http://127.0.0.1:8000/api/auth/user/')
-    //             .then(response => {
-    //                 setUser(response.data);
-    //                 setIsAuthenticated(true);
-    //             })
-    //             .catch(error => {
-    //                 console.error('Error fetching user:', error);
-    //                 setIsAuthenticated(false);
-    //             });
-    //     }
-    // }, []);
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            axios.get('http://127.0.0.1:8000/api/auth/user/')
+                .then(response => {
+                    setUser(response.data);
+                    setIsAuthenticated(true);
+                })
+                .catch(error => {
+                    console.error('Error fetching user:', error);
+                    setIsAuthenticated(false);
+                });
+        }
+    }, []);
 
     return (
         <AuthContext.Provider value={{ user, isAuthenticated, register, login, logout }}>
